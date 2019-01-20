775227ee4cc54c7902daae88948eaacbf6036ea0
biancacristtorres <biancacristtorres34@gmail.com>
2019-01-20 01:24:23 -0200

    finalizacao de cenarios

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# teste
+# O Projeto
+
+## Pré requisitos
+
+* Instalar Ruby + DevKit 
+* Instalar o Bundler e executá-lo
+``` 
+gem install bundler
+```
+O comando abaixo deve ser executado na raiz do projeto, onde se encontra a *Gemfile*
+``` 
+bundle install
+```
+* Firefox **versão 42** disponível para download em https://mzl.la/2R15I38
+
+## Execução
+
+* Comando para execução dos testes
+``` 
+cucumber
+``` 
+Este comando também deve ser executado na raiz do projeto
--- features/step_definitions/busca.rb
+++ features/step_definitions/busca.rb
@@ -1,62 +1,52 @@
-require 'selenium-webdriver'
 require 'cucumber'
 require 'rspec'
-require 'pry'
-
-Selenium::WebDriver::Chrome.driver_path="C:/projetos/anima/qa-engineer-challenge/chromedriver_win32/chromedriver.exe"
 
 Given(/^navego para a página de busca do banco de questões$/) do
-  #Declarando a variável @Driver atribuindo o Webdriver do Chrome.
-  @driver = Selenium::WebDriver.for :chrome
-  @driver.get "https://opentdb.com/browse.php"
+  $driver.get "https://opentdb.com/browse.php"
   sleep 3
 end
 
-Given(/^digito "([^"]*)" no campo de busca$/) do |arg1|
-  @driver.find_element(:id, "query").send_keys arg1
+Given(/^digito "([^"]*)" no campo de busca$/) do |frasePesquisa|
+  $driver.find_element(:id, "query").send_keys frasePesquisa
   sleep 3
 end
 
 When(/^clico no botão de buscar$/) do
-  @driver.find_element(:xpath, "//button[@class='btn btn-sm btn-default']").click
-end
-
-Then(/^deve aparecer uma mensagem de erro com o texto "([^"]*)"$/) do |arg1|
-  mensage =  @driver.find_element(:xpath, "//div[@class='alert alert-danger']").text
-  expect(mensage).to eq(arg1)
-end
-
-
-#cenario 2
-Given(/^seleciono o valor "([^"]*)" no campo do tipo de busca$/) do |arg1|
-  @driver.find_element(:id, "type").find_element(:css,"option[value='Category']").click
+  $driver.find_element(:xpath, "//button[@class='btn btn-sm btn-default']").click
   sleep 3
 end
 
-Then(/^deve aparecer a listagem de questões com (\d+) itens$/) do |arg1|
-  table = @driver.find_elements(:tag_name, "tr").size - 1
-  expect(table.to_s).to eq(arg1.to_s)  
+Then(/^deve aparecer uma mensagem de erro com o texto "([^"]*)"$/) do |notFound|
+  expect($driver.find_element(:xpath, "//div[@class='alert alert-danger']").text).to eq(notFound.to_s)
+  sleep 3
+end
+
+Given(/^seleciono o valor "([^"]*)" no campo do tipo de busca$/) do |category|
+  $driver.find_element(:id, "type").find_element(:css,"option[value='#{category}']").click
+  sleep 3
+end
+
+Then(/^deve aparecer a listagem de questões com (\d+) itens$/) do |qtdQuest|
+  expect($driver.find_elements(:tag_name, "tr").size - 1).to eq(qtdQuest.to_i)  
+  sleep 3
 end
 
 Then(/^deve aparecer o controle de paginação$/) do
-  @driver.find_element(:xpath, "//center/ul[@class='pagination pagination-lg']").displayed?
+  $driver.find_element(:xpath, "//center/ul[@class='pagination pagination-lg']").displayed?
+  sleep 3
 end
 
-Then(/^deve aparecer a listagem de questões com menos de (\d+) itens$/) do |arg1|
-  @driver.find_elements(:tag_name, "tr").size - 1 < 26
+Then(/^deve aparecer a listagem de questões com menos de (\d+) itens$/) do |qtdQuest|
+  expect($driver.find_elements(:tag_name, "tr").size - 1).to be < qtdQuest.to_i
+  sleep 3
 end
 
 Then(/^não deve aparecer o controle de paginação$/) do
-    begin
-      expect(@driver.find_element(:xpath, "//center/ul[@class='pagination pagination-lg']").displayed?).to eql false
+	begin
+    expect($driver.find_element(:xpath, "//center/ul[@class='pagination pagination-lg']").displayed?).to eql false
     rescue Selenium::WebDriver::Error::NoSuchElementError
       false
     rescue Selenium::WebDriver::Error::StaleElementReferenceError
       false
     end
-end
-
-
-
-
-  
+end  

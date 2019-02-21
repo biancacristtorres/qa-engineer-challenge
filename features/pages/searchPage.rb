cf0537cdb07ec190c4ff2ec55b65fde58207cfeb
Bianca Cristina Torres Silva <bianca.torres@animaeducacao.com.br>
2019-02-21 14:02:02 -0300

    pageObjects criado

--- features/pages/searchPage.rb
+++ features/pages/searchPage.rb
@@ -1,46 +1,54 @@
 class SearchPage
 
-    #frase = $driver.find_element(:id, "query")
-    frase = {id: "query"}
-    searchBtn = {xpath: "//button[@class='btn btn-sm btn-default']"}
-    #searchBtn = $driver.find_element(:xpath, "//button[@class='btn btn-sm btn-default']")
-    #mensageNotFound = $driver.find_element(:xpath, "//div[@class='alert alert-danger']").text
-    mensageNotFound = {xpath: "//div[@class='alert alert-danger']"}
-    #categoryBtn = $driver.find_element(:id, "type").find_element(:css,"option[value='#{category}']")
-    categoryBtn = {id: "type"}
-    questionsList = {tag_name: "tr"}
-    # questionsList = $driver.find_elements(:tag_name, "tr")
-    # controlPage = $driver.find_element(:xpath, "//center/ul[@class='pagination pagination-lg']")
-    controlPage = {xpath: "//center/ul[@class='pagination pagination-lg']"}
-
-
-    def sendKeysFrase(texto)
-        #frase.send_keys texto
-        $driver.find_element(frase).send_keys texto
+    #mapeia os elementos
+    def campoBusca
+        $driver.find_element(:id, "query")
     end
     
-    def clickSearch
-        # searchBtn.click
-        $driver.find_element(searchBtn).click
+    def botaoSearch
+        $driver.find_element(:xpath, "//button[@class='btn btn-sm btn-default']")
     end
 
-    def returnMensageNotFound
-        # mensageNotFound.texto
-        $driver.find_element(mensageNotFound).text
+    def mensageNotFound
+        $driver.find_element(:xpath, "//div[@class='alert alert-danger']")
     end
 
-    def clickCategory
-        # categoryBtn.click
-        $driver.find_element(categoryBtn).find_element(:css,"option[value='#{category}']").click
+    def campoValor(category)
+        $driver.find_element(:id, "type").find_element(:css,"option[value='#{category}']")
     end
 
-    def questionsWith
-        #questionsList.size - 1
-        $driver.find_element(questionsList).size - 1
+    def listaQuestoes
+        $driver.find_elements(:tag_name, "tr")
     end
     
-    def controlPageDisplayed
-        $driver.find_element(controlPage).displayed?
+    def paginacao
+        $driver.find_element(:xpath, "//center/ul[@class='pagination pagination-lg']")
     end
-    
+
+
+    #realiza as ações
+    def preencherCampo(texto)
+        campoBusca.send_keys texto
+    end
+
+    def clickPesquisar
+        botaoSearch.click
+    end
+
+    def mensagemNEntrontrado
+        mensageNotFound.text
+    end
+
+    def clickCategoria(category)
+        campoValor(category).click
+    end
+
+    def countDeQuestoes
+        listaQuestoes.size - 1
+    end
+
+    def exibePaginacao
+        paginacao.displayed?
+    end
+
 end
